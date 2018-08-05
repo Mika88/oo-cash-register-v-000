df8ef7eb83677ba6748ceb20e06debf9aded00d9
Mika88 <mikaspivak@gmail.com>
2018-08-05 19:22:12 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
     self.total += new_price
   end
   def apply_discount
-  discount_a = total.to_f / 100 * 20
-  total.to_f - discount_a
+  discount_a = total / 100 * discount
+  total - discount_a
   end
 end
